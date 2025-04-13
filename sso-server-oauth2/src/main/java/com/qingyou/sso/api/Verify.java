0aed7c131eaef9a17a57a33571004e519dc6261f
feellmoose <feell4429@163.com>
2025-04-13 10:19:28 +0800

    feat: simple login page

--- sso-server-oauth2/src/main/java/com/qingyou/sso/api/Verify.java
+++
@@ -1,7 +0,0 @@
-package com.qingyou.sso.api;
-
-import io.vertx.ext.web.RoutingContext;
-
-public interface Verify {
-    void verify(RoutingContext routingContext);
-}
--- sso-site/sso-client-site/src/App.tsx
+++ sso-site/sso-client-site/src/App.tsx
@@ -1,29 +1,16 @@
-import { useState } from 'react'
-import siteLogo from '/logo.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css'
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://feellmoose.fun" target="_blank">
-          <img src={siteLogo} className="logo" alt="Site Dev" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
